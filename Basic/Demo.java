9b0bcd3c7302bc9f1d57964013914b206ec05f1d
imamanjangra <imamanjangra@gmail.com>
2025-10-16 23:19:44 +0530

    learn basic

---
+++ Basic/Demo.java
@@ -0,0 +1,104 @@
+// class Demo {
+//     public static void main(String[] args) {
+//         String name = "Aman";
+//         System.out.println(name + "Jangra");
+        
+//     }
+// }
+
+// ----------------------Types of Print statement---------------------------------------
+
+// public class Demo {
+
+//     public static void main(String[] args) {
+//         System.out.println("Aman");// print to the next line 
+//         System.out.println("jangra");
+
+
+//         System.out.print("Tarun");// print in same line 
+//         System.out.print(81);// print in same line 
+//     }
+// }
+
+// --------------------------Types of variables -----------------------------------------
+
+/* Types of variables in java
+1) String
+2) int
+3)float
+4) char
+5) boolean
+
+*/ 
+
+// final variable 
+
+// public class Demo {
+
+//     public static void main(String[] args) {
+//        final int num1 = 50; // final or constant not overright the exsiting content this is final value 
+//         System.out.println(num1);
+//         System.out.println("number is = " + num1);
+//         System.out.println("number is = " + num1 + 3); // if we add ito string then all data is considered into string so result is come ito string 
+//         System.out.println("number is = " + (num1 + 3));// in which add into number 
+
+//         // multiple assigment 
+//         int a , b , c ;
+//         a = b = c = 20;
+//         System.err.println(a + b + c);
+//     }
+// }
+
+// ex 
+
+// public class Demo {
+
+//     public static void main(String[] args) {
+//         int num1 = 10;
+//         int num2 = 2;
+
+//         // calculate area of reactangle
+
+//         int area = num1 * num2;
+//         System.out.println("Area of rectangle is = " + area);
+//     }
+// }
+
+/* --------------------------TYPES OF DATA ----------------------------------
+                1) Primitive data            2) NON-Primitive data
+
+                    1)Primitive data -> includes byte, short, int, long, float, double, boolean and char
+                                a) integer type                  b) float type 
+
+                                    a) integer number -> Integer types stores whole numbers, positive or negative ->byte, short, int , long
+                                    b) float type -> numbers with a fractional part, containing one or more decimals. -> float , double 
+
+                    2)Non-Primitive data -> such as String, Arrays and Classes
+
+*/ 
+
+//  ----------------------------------  var KEYWORD-----------------------------------
+
+// public class Demo {
+
+//     public static void main(String[] args) {
+//         var num = 12;  // is same as js it auto conver data type of give data 
+//         System.out.println(num);
+//         var str = "Aman";
+//         System.out.println(str);
+
+//     }
+// }
+
+/*   -----------------------------------------type casting -----------------------------------------------
+        1) Widening Casting -> smaller to larger (automatic)
+        2) Narrowing Casting -> larger to smaller (Manual)
+*/
+
+public class Demo {
+
+    
+}
+
+
+
